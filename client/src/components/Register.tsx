3f987ce79cfea98663bc4ec2ea382a8485335f51
Elisheva Einhoren <shevibr0@gmail.com>
2023-11-23 22:37:59 +0200

    continue with register

--- client/src/components/Register.tsx
+++ client/src/components/Register.tsx
@@ -1,25 +1,37 @@
-import axios from 'axios'
+import axios, { AxiosError } from 'axios'
 import { useObjectState } from 'mg-js'
-import React from 'react'
-
+import React, { useState } from 'react'
+import { useForm, SubmitHandler } from "react-hook-form"
+type Inputs = {
+  name: string,
+  email: string,
+  password: string
+}
 const Register = () => {
-  const [data, setData] = useObjectState(["name", "email", "password"])
-  console.log(data)
-  const register = async (e: any) => {
-    e.preventDefault()
-    const { data: resp } = await axios.post("http://localhost:3000/users/register", data)
-    console.log(resp)
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<Inputs>()
+  const onSubmit: SubmitHandler<Inputs> = (data) => {
+    console.log(data)
   }
+  const genError = (err: string, option: React.CSSProperties) => <span style={option}>{err}</span>
   return (
     <div>
-      <form onSubmit={register}>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <label>name:</label>
-        <input type='text' value={data.name} onChange={(e) => setData("name", e.target.value)}></input>
+        <input type='text'{...register("name", { required: true })}></input>
+        {errors.name && genError("name is required", {
+          color: "red"
+        })}
         <label>email:</label>
-        <input type='email' value={data.email} onChange={(e) => setData("email", e.target.value)}></input>
+        <input type='email' {...register("email", { required: true })} ></input>
         <label>password:</label>
-        <input type='number' value={data.password} onChange={(e) => setData("password", e.target.value)}></input>
+        <input type='number' {...register("password", { required: true })}></input>
         <button type='submit'>Send</button>
+
       </form>
     </div >
   )
