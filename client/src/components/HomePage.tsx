aa18397da2c42f047c68ed984deb08f87d820f93
Elisheva Einhoren <shevibr0@gmail.com>
2023-12-08 09:57:05 +0200

    design homepage

--- client/src/components/HomePage.tsx
+++ client/src/components/HomePage.tsx
@@ -8,12 +8,13 @@ const HomePage = () => {
       <img src="src/assents/profile.jpg" alt="Your Alt Text" className="w-60 h-60 rounded-full mr-8" />
 
       <div className="bg-transparent p-8 rounded-lg shadow-md w-96 h-100 relative border-2 border-orange-400">
-        <div>
+        <div style={{ direction: 'rtl', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', textAlign: 'center' }} >
           <br />
+          <h2 className="text-xl font-semibold mb-2 text-purple-700 textAlign:center">מפה לעו"ס</h2>
           <p className="border-orange-400 text-lg mb-4" style={{ direction: 'rtl', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', textAlign: 'center' }}>
             הי, שמי אלישבע אינהורן, עובדת סוציאלית ומפתחת תוכנה. עבדתי בעבר 6 שנים בלשכת רווחה
             .במהלך שנות עבודתי נתקלתי רבות בצורך בחיפוש אחר מוסדות כמו פנימיות, מעונות יום, מועדוניות ועוד
-            אך לא מצאתי אתר מסודר בו ניתן לחפש את כל מוסדות הרווחה בארץ ולקרוא תגובות וחוות של עו"סיות אחרות
+            אך לא מצאתי אתר מסודר בו ניתן לחפש את כל מוסדות הרווחה בארץ ולקרוא תגובות וחוות דעת של עו"סיות אחרות
             על המקום. בסיום לימודי פיתוח התוכנה החלטתי לבנות מענה לצורך עבור עו"סים ועו"סיות בכל רחבי הארץ אשר בכוח השיתוף
             והדרוג של מוסדות שביקרו בהם יעזרו לעו"סים אחרים ויוכלו להנות ממידע מעודכן ומאימן.
             מוזמנת להוסיף חוות דעת ודרוג אודות מוסדות שהכרת באופן אישי ולהנות ממידע המתעדכן כל הזמן
@@ -24,7 +25,7 @@ const HomePage = () => {
           </button>
         </div>
       </div>
-    </div>
+    </div >
   );
 };
 
